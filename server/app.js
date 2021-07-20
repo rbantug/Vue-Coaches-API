c1c35b4163fe285c59997cffa4f78c2edfbc3802
rbantug <ronbantug22@gmail.com>
2021-07-20 17:19:36 +0800

    trying to deploy to heroku

--- server/app.js
+++ server/app.js
@@ -1,31 +1,37 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 
-const allRoutes = require("./routes/allRoutes");
-const errorController = require("./controllers/errorController");
+const allRoutes = require('./routes/allRoutes');
+const errorController = require('./controllers/errorController');
 
 const app = express();
 
 app.use(
-	cors({
-		origin: ["http://192.168.1.13:8080", "https://192.168.1.13:8080"],
-		exposedHeaders: ["authorization", "set-cookie"],
-		credentials: true,
-	})
+  cors({
+    /* origin: ['http://192.168.1.13:8080', 'https://192.168.1.13:8080'], */
+    exposedHeaders: ['authorization', 'set-cookie'],
+    credentials: true
+  })
 );
 //app.options("*", cors());
 app.use(helmet());
 
-if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: '10kb' }));
 
 app.use(cookieParser());
 
-app.use("/api/v1", allRoutes);
+app.use('/api/v1', allRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(`${__dirname}/public`));
+
+  app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+}
 
 app.use(errorController);
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,99 +1,99 @@
-const jwt = require("jsonwebtoken");
-const { promisify } = require("util");
-const catchAsyncErrors = require("../utils/catchAsyncErrors");
-const AppError = require("../utils/appError");
-const User = require("../models/UserModel");
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+const catchAsyncErrors = require('../utils/catchAsyncErrors');
+const AppError = require('../utils/appError');
+const User = require('../models/UserModel');
 
-const signToken = (id) => {
-	return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
-		expiresIn: process.env.JWT_EXPIRES_IN,
-	});
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
 };
 
 const sendToken = (user, statusCode, req, res) => {
-	const token = signToken(user._id);
+  const token = signToken(user._id);
 
-	res.cookie("jwt", token, {
-		expires: new Date(
-			Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 3600000
-		),
-		httpOnly: true,
-		sameSite: "none",
-		secure: req.secure || req.headers["x-forwarded-proto"] === "https",
-	});
+  res.cookie('jwt', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 3600000
+    ),
+    httpOnly: true,
+    sameSite: 'secure',
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+  });
 
-	user.password = undefined;
+  user.password = undefined;
 
-	res.status(statusCode).json({
-		status: "success",
-		token,
-		data: {
-			user,
-		},
-	});
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  });
 };
 
 exports.signup = catchAsyncErrors(async (req, res, next) => {
-	const newUser = await User.create({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password,
-		confirmPassword: req.body.confirmPassword,
-		passwordChangedAt: req.body.passwordChangedAt,
-	});
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    passwordChangedAt: req.body.passwordChangedAt
+  });
 
-	sendToken(newUser, 201, req, res);
+  sendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsyncErrors(async (req, res, next) => {
-	const { email, password } = req.body;
+  const { email, password } = req.body;
 
-	// check if email and password exist
-	if (!email || !password) {
-		return next(new AppError("Please provide an email and a password", 400));
-	}
+  // check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide an email and a password', 400));
+  }
 
-	// check if email and password are correct
-	const user = await User.findOne({ email }).select("+password");
-	if (!user || !(await user.comparePasswords(password, user.password))) {
-		return next(new AppError("Incorrect email or password", 401));
-	}
-	// sendToken
-	sendToken(user, 200, req, res);
+  // check if email and password are correct
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.comparePasswords(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  // sendToken
+  sendToken(user, 200, req, res);
 });
 
 exports.protect = catchAsyncErrors(async (req, res, next) => {
-	let token;
+  let token;
 
-	// get the token and check if it exist
-	if (
-		req.headers.authorization &&
-		req.headers.authorization.startsWith("Bearer")
-	) {
-		token = req.headers.authorization.split(" ")[1];
-	} else if (req.cookies.jwt) {
-		token = req.cookies.jwt;
-	}
+  // get the token and check if it exist
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
 
-	if (!token) {
-		return next(new AppError("You are not logged in.", 401));
-	}
-	// verify token & check if user still exist
-	const decodedData = await promisify(jwt.verify)(
-		token,
-		process.env.JWT_SECRET_KEY
-	);
-	const currentUser = await User.findById(decodedData.id);
-	if (!currentUser) {
-		return next(new AppError("This user does not exist", 401));
-	}
+  if (!token) {
+    return next(new AppError('You are not logged in.', 401));
+  }
+  // verify token & check if user still exist
+  const decodedData = await promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET_KEY
+  );
+  const currentUser = await User.findById(decodedData.id);
+  if (!currentUser) {
+    return next(new AppError('This user does not exist', 401));
+  }
 
-	// check if the user has changed the password after issuing the JWT
-	if (currentUser.changedPasswordAfter(decodedData.iat)) {
-		return next(new AppError("The user recently changed the password", 401));
-	}
+  // check if the user has changed the password after issuing the JWT
+  if (currentUser.changedPasswordAfter(decodedData.iat)) {
+    return next(new AppError('The user recently changed the password', 401));
+  }
 
-	// store user info in req.user
-	req.user = currentUser;
-	next();
+  // store user info in req.user
+  req.user = currentUser;
+  next();
 });
